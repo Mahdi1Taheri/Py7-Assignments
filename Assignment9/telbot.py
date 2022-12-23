4ab237960495f475b2ff15c6059d0167e798e952
Mahdi Taheri <96267873+Mahdi1Taheri@users.noreply.github.com>
2022-12-24 00:40:55 +0330

    Update telbot.py

--- Assignment9/telbot.py
+++ Assignment9/telbot.py
@@ -2,12 +2,14 @@ import random
 import telebot
 from telebot import types
 from khayyam import JalaliDate, JalaliDatetime 
+import gtts
+import qrcode
 
 markup = types.ReplyKeyboardMarkup(row_width=2)
 itembtn1 = types.KeyboardButton('/New_Game')
 markup.add(itembtn1)
 
-bot = telebot.TeleBot(<TOKEN>, parse_mode=None)
+bot = telebot.TeleBot("5855476837:AAFic6w9zD1XPESnfDCoUyNQiBo5fdRQHRg", parse_mode=None)
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 
@@ -40,7 +42,7 @@ def game(message):
 	guess1 = bot.send_message(message.chat.id, "Enter a number between 0 and 40:" , reply_markup=markup)
 	bot.register_next_step_handler(guess1,guess)
 
-@bot.message_handler(func=lambda m: True)
+
 def guess(message):
 	isint = (message.text).isdigit()
 	if isint == False:
@@ -64,9 +66,62 @@ def birth(message):
   bot.register_next_step_handler(date, age)
 
 def age(message):
-  birthday = str(message.text).split()
-  age1 = JalaliDatetime.now() - JalaliDatetime(birthday[0], birthday[1], birthday[2]) // 365
-  bot.send_message(message.chat.id,"Your age is: ", age1)
-		
+  birthDate = message.text.split("/")
+  difference = JalaliDatetime.now() - JalaliDatetime(birthDate[0], birthDate[1], birthDate[2])
+  year = difference.days // 365
+  difference = difference.days % 365
+  month = difference // 30
+  day = difference % 30 - 7
+  bot.send_message(message.chat.id, "your age: " + str(year) + " years and " + str(month) + " months and " + str(day) + " days")
 
+@bot.message_handler(commands= ["voice"])
+def voice(message):
+	msg = bot.send_message(message.chat.id,"Enter a text to convert in voice.")
+	bot.register_next_step_handler(msg,convert)
+def convert(message):
+  sound2 = gtts.gTTS(text = message.text , lang = "en" , slow = False )
+  sound2.save("VoiceBot.ogg")
+  audio = open("VoiceBot.ogg", 'rb')
+  bot.send_audio(message.chat.id, audio)
+
+@bot.message_handler(commands= ["max"])
+def input_array(message):
+	msg = bot.send_message(message.chat.id,"Please Enter an array to find the max number.(e.g: 2,3,4,1,7,0 = 7)")
+	bot.register_next_step_handler(msg,max_array)
+
+def max_array(message):
+	text = message.text.split(",")
+	max = 0
+	for num in text:
+		if max < int(num):
+			max = int(num)
+	bot.send_message(message.chat.id, f"The maximum number of your array is {max}")
+ 
+@bot.message_handler(commands= ["argmax"])
+def input_array(message):
+	msg = bot.send_message(message.chat.id,"Please Enter an array to find the index of max number.(e.g: 2,3,4,1,7,0 = 4)")
+	bot.register_next_step_handler(msg,argmax)
+
+def argmax(message):
+	text = message.text.split(",")
+	max = 0
+	ind = 0
+	for num in text:
+		if max < int(num):
+			max = int(num)
+			index = ind
+		ind += 1
+	bot.send_message(message.chat.id, f"The maximum index number of your array is {index}")
+ 
+@bot.message_handler(commands= ["qrcode"]) 
+def input_qr(message):
+	data = bot.send_message(message.chat.id,"write a text to convert it to qrcode.")
+	bot.register_next_step_handler(data,qr)
+
+def qr(message):
+	img = qrcode.make(message.text)
+	img.save("qrcode.png")
+	img2 = open("qrcode.png","rb")
+	bot.send_photo(message.chat.id, img2)
+ 
 bot.infinity_polling()
