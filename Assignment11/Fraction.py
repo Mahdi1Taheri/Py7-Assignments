e08dfa90a3a560da329eaea487585f7aa5252308
Mahdi Taheri <96267873+Mahdi1Taheri@users.noreply.github.com>
2023-01-09 14:06:00 +0330

    some bugs in methods fixed.

--- Assignment11/Fraction.py
+++ Assignment11/Fraction.py
@@ -8,29 +8,28 @@ class Fraction:
 
     
     # Methods
-    def multiply(self,Kasr1,Kasr2): # ضرب کسر
-        result_s = Kasr1.s * Kasr2.s
-        result_m = Kasr1.m * Kasr2.m
+    def multiply(self,other): # ضرب کسر
+        result_s = self.s * other.s
+        result_m = self.m * other.m
         x = Fraction(result_s,result_m)
         return x
     
-    def division(self,Kasr1,Kasr2): # تقسیم کسر
-        result_s = Kasr1.s * Kasr2.m
-        result_m = Kasr1.m * Kasr2.s
-        
+    def division(self,other): # تقسیم کسر
+        result_s = self.s * other.m
+        result_m = self.m * other.s
         x = Fraction(result_s,result_m)
         return x
     
-    def sum(self,K1,K2): # جمع کسر
-        s = K1.s * K2.m + K1.m * K2.s
-        m = K1.m * K2.m
+    def sum(self,other): # جمع کسر
+        s = self.s * other.m + self.m * other.s
+        m = self.m * other.m
         x = Fraction(s,m)
         return x
 
     
-    def subtraction(self,K1,K2): # تفریق کسر
-        s = K1.s * K2.m - K1.m * K2.s
-        m = K1.m * K2.m
+    def subtraction(self,other): # تفریق کسر
+        s = self.s * other.m - self.m * other.s
+        m = self.m * other.m
         x = Fraction(s,m)
         return x
 
@@ -52,10 +51,5 @@ class Fraction:
 a = Fraction(8,2)
 b = Fraction(9,3)
 
-
-
-
-
-
-
-    
+z = a.sum(b)
+z.show()
