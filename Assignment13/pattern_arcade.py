8985269da603cb2183f6411ffdc06046d035eb2f
Mahdi Taheri <96267873+Mahdi1Taheri@users.noreply.github.com>
2023-01-15 14:25:35 +0330

    Update pattern_arcade.py

--- Assignment13/pattern_arcade.py
+++ Assignment13/pattern_arcade.py
@@ -1,4 +1,3 @@
-# Library imports
 import arcade
 
 COLUMN_SPACING = 20
@@ -11,12 +10,8 @@ arcade.set_background_color(arcade.color.WHITE)
 
 arcade.start_render()
 
-# Loop for each row
 for row in range(10):
-    # Loop for each column
-    # Change the number of columns depending on the row we are in
     for column in range(10):
-        # Calculate our location
         x = column * COLUMN_SPACING + LEFT_MARGIN
         y = row * ROW_SPACING + BOTTOM_MARGIN
 
@@ -32,10 +27,5 @@ for row in range(10):
             elif column % 2 != 0:
                 arcade.draw_rectangle_filled(x,y,10,10,arcade.color.RED,45)
 
-# Finish the render.
 arcade.finish_render()
-
-# Keep the window up until someone closes it.
 arcade.run()
-    
-        
